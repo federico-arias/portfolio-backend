955ce7b441ab1c008e639a6ef9940cc32e595625
Federico Arias <federicoariasr@gmail.com>
2022-01-31 22:12:37 +0100

    [rm] unnecesary files

--- src/postgres/index.ts
+++
@@ -1,9 +0,0 @@
-import { Pool } from "pg"
-
-export const getDatabaseConnection = async (
-	connectionString: string,
-): Promise<Pool> => {
-	const client = new Pool({ connectionString })
-	await client.connect()
-	return client
-}
